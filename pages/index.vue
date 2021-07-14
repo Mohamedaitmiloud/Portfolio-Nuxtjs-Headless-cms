6e562e8d1f9ce33475d31020b3aa530f74b112f0
Mohamed Ait Miloud <mohammed.aitmiloud@gmail.com>
2021-07-14 22:34:40 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     <Header @darkMode="ChangeMode" :mode="lightMode"/>
     <Main />
     <Footer />
+    <div class="elfsight-app-3e4f01b1-a420-4a8e-9e57-a685c0a78d5e"></div>
   </div>
 </template>
 
