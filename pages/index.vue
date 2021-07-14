bf139a77a944ca4b28ced67a82c771990ed4d714
Mohamed Ait Miloud <mohammed.aitmiloud@gmail.com>
2021-07-14 22:39:57 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,8 @@
   <div :class = "(lightMode)?'yb-dark-skin uk-light':''">
     <Header @darkMode="ChangeMode" :mode="lightMode"/>
     <Main />
+  <div class="elfsight-app-3e4f01b1-a420-4a8e-9e57-a685c0a78d5e"></div>
     <Footer />
-    <div class="elfsight-app-3e4f01b1-a420-4a8e-9e57-a685c0a78d5e"></div>
   </div>
 </template>
 
